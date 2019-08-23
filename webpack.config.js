c2a649ae7f859fa1b9fa182fefea622cebaf65f2
d-issy <shepabashi@gmail.com>
2019-08-24 07:59:49 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -28,9 +28,7 @@ Encore
     .addEntry('style', './assets/css/style.sass')
 
     // javascript
-    .addEntry('main', './assets/js/main.ts')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addEntry('main', './assets/js/main.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
